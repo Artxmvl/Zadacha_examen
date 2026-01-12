63ad32484715be53ff4699d4703e049bb1aada70
Artxmvl <talakhovartem2007@gmail.com>
2026-01-12 19:29:49 +0300

    Добавить Menu и ReportGenerator заголовки

--- src/Payment.cpp
+++ src/Payment.cpp
@@ -6,8 +6,10 @@ CardPayment::CardPayment(const string& cardNumber)
     : cardNumber(cardNumber) {}
 
 bool CardPayment::pay(double amount) {
-    cout << "Processing card payment of " << amount << " rubles..." << endl;
-    cout << "Card ending in " << cardNumber.substr(cardNumber.length() - 4) << " charged." << endl;
+    cout << "Processing card payment..." << endl;
+    cout << "Amount: " << amount << " rubles" << endl;
+    cout << "Card: **** **** **** " << cardNumber.substr(cardNumber.length() - 4) << endl;
+    cout << "Payment successful!" << endl;
     return true;
 }
 
@@ -15,8 +17,10 @@ EWalletPayment::EWalletPayment(const string& walletId)
     : walletId(walletId) {}
 
 bool EWalletPayment::pay(double amount) {
-    cout << "Processing e-wallet payment of " << amount << " rubles..." << endl;
-    cout << "Wallet " << walletId << " charged." << endl;
+    cout << "Processing e-wallet payment..." << endl;
+    cout << "Amount: " << amount << " rubles" << endl;
+    cout << "Wallet ID: " << walletId << endl;
+    cout << "Payment successful!" << endl;
     return true;
 }
 
@@ -24,7 +28,9 @@ SBPPayment::SBPPayment(const string& phoneNumber)
     : phoneNumber(phoneNumber) {}
 
 bool SBPPayment::pay(double amount) {
-    cout << "Processing SBP payment of " << amount << " rubles..." << endl;
-    cout << "Phone " << phoneNumber << " charged." << endl;
+    cout << "Processing SBP payment..." << endl;
+    cout << "Amount: " << amount << " rubles" << endl;
+    cout << "Phone: " << phoneNumber << endl;
+    cout << "Payment successful!" << endl;
     return true;
 }
